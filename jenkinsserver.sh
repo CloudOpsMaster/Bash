1412a698b39f90e7779e752ffe174e41f421e85d
Maks0123 <VadimCorporateUkr@gmail.com>
2021-08-31 14:57:18 +0300

    Update and rename installJenkins.sh to jenkinsserver.sh

---
+++ jenkinsserver.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add 
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install jenkins
+sudo systemctl start jenkins
+sudo ufw allow OpenSSH
+sudo ufw enable
+sudo ufw allow 8080
+sudo ufw status
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+sudo systemctl status jenkins
