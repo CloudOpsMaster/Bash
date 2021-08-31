828e76a48e0313c3e58c299656b6b3c46319ca37
Maks0123 <VadimCorporateUkr@gmail.com>
2021-08-31 15:04:53 +0300

    Update jenkinsserver.sh

--- jenkinsserver.sh
+++ jenkinsserver.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#Updating software repositories
+sudo apt update && sudo apt upgrade -y
+
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add 
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt update
